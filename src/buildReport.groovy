0db6bb89619bc47c283f1dec63274f4581b9c504
Morten Nilsen <morten@runsafe.no>
2024-01-06 00:06:42 +0100

    Add discord notifier

---
+++ src/buildReport.groovy
@@ -0,0 +1,22 @@
+#!/usr/bin/env groovy
+
+/**
+ * Send notifications based on build status string
+ */
+def call(String repository) {
+
+  // Send notification
+	discordSend (
+		description: 'Build finished',
+		enableArtifactsList: true,
+		footer: '',
+		image: '',
+		link: env.BUILD_URL,
+		result: currentBuild.currentResult,
+		scmWebUrl: "https://github.com/Runsafe/${repository}/commit/%s",
+		showChangeset: true,
+		thumbnail: '',
+		title: "${env.JOB_NAME} ${env.BUILD_NUMBER}",
+		webhookURL: env.DISCORD_WEBHOOK
+	)
+}
