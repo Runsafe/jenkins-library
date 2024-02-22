d8e96ff73984092b70c30f8ff7f92b3fe7e0d9ef
Morten Nilsen <morten@runsafe.no>
2024-02-23 00:05:20 +0100

    Write build report to builds.txt on installation

--- vars/installPlugin.groovy
+++ vars/installPlugin.groovy
@@ -3,12 +3,13 @@
 /**
  * Install plugin to server
  */
-def call(String archive)
+def call(String archive, String job, String name)
 {
 	dir('artifacts')
 	{
 		unstash 'archive'
 		sh 'mkdir -p ~/bukkit/plugins'
 		sh "tar -xvf ${archive} -C ~/bukkit/plugins"
+		sh "echo -e \"[\\033[0;32m$(date +%T)\\033[m] \\033[0;36m${job} \\033[1;36m${name} \\033[1;32mbuild completed\\033[m.\" >> ~/builds.txt"
 	}
 }
