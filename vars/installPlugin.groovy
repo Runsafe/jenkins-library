e5f782b5c5e7598999b94cb9ffeda264d4edc992
Morten Nilsen <morten@runsafe.no>
2024-02-23 00:36:20 +0100

    Fix script

--- vars/installPlugin.groovy
+++ vars/installPlugin.groovy
@@ -3,13 +3,13 @@
 /**
  * Install plugin to server
  */
-def call(String archive, String job, String name)
+def call(String archive)
 {
 	dir('artifacts')
 	{
 		unstash 'archive'
 		sh 'mkdir -p ~/bukkit/plugins'
 		sh "tar -xvf ${archive} -C ~/bukkit/plugins"
-		sh "echo -e \"[\\033[0;32m\$(date +%T)\\033[m] \\033[0;36m${job} \\033[1;36m${name} \\033[1;32mbuild completed\\033[m.\" >> ~/builds.txt"
+		sh "echo -e \"[\\033[0;32m\$(date +%T)\\033[m] \\033[0;36m${currentBuild.fullProjectName} \\033[1;36m${currentBuild.displayName} \\033[1;32mbuild completed after ${startTimeInMillis}ms\\033[m.\" >> ~/builds.txt"
 	}
 }
