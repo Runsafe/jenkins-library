89443e32b77a39f73d3797f57e82b3d09df80089
Morten Nilsen <morten@runsafe.no>
2024-02-23 00:44:09 +0100

    Calculate time elapsed

--- vars/installPlugin.groovy
+++ vars/installPlugin.groovy
@@ -10,6 +10,6 @@ def call(String archive)
 		unstash 'archive'
 		sh 'mkdir -p ~/bukkit/plugins'
 		sh "tar -xvf ${archive} -C ~/bukkit/plugins"
-		sh "echo -e \"[\\033[0;32m\$(date +%T)\\033[m] \\033[0;36m${currentBuild.fullProjectName} \\033[1;36m${currentBuild.displayName} \\033[1;32mbuild completed after ${currentBuild.startTimeInMillis}ms\\033[m.\" >> ~/builds.txt"
+		sh "echo -e \"[\\033[0;32m\$(date +%T)\\033[m] \\033[0;36m${currentBuild.fullProjectName} \\033[1;36m${currentBuild.displayName} \\033[1;32mbuild completed after \$((\$(date +%s%N)/1000000-${currentBuild.startTimeInMillis}))ms\\033[m.\" >> ~/builds.txt"
 	}
 }
