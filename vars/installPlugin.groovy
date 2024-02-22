c7b1b4176b97c443f28add77277ba48f930efc8c
Morten Nilsen <morten@runsafe.no>
2024-02-23 00:24:26 +0100

    escape $ in shell script

--- vars/installPlugin.groovy
+++ vars/installPlugin.groovy
@@ -10,6 +10,6 @@ def call(String archive, String job, String name)
 		unstash 'archive'
 		sh 'mkdir -p ~/bukkit/plugins'
 		sh "tar -xvf ${archive} -C ~/bukkit/plugins"
-		sh "echo -e \"[\\033[0;32m$(date +%T)\\033[m] \\033[0;36m${job} \\033[1;36m${name} \\033[1;32mbuild completed\\033[m.\" >> ~/builds.txt"
+		sh "echo -e \"[\\033[0;32m\$(date +%T)\\033[m] \\033[0;36m${job} \\033[1;36m${name} \\033[1;32mbuild completed\\033[m.\" >> ~/builds.txt"
 	}
 }
