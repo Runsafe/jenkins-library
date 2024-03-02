4edfe30bfd4464bf014274552b25d8f5c601c0f0
Morten Nilsen <morten@runsafe.no>
2024-03-02 22:36:58 +0100

    Add missing escape of $ in script

--- vars/stagePlugin.groovy
+++ vars/stagePlugin.groovy
@@ -10,6 +10,6 @@ def call(String archive)
 		unstash 'archive'
 		sh 'mkdir -p ~/updates'
 		sh "tar -xvf ${archive} -C ~/updates"
-		sh "echo -e \"[\\033[0;32m$(date +%T)\\033[m] \\033[0;36m${currentBuild.fullProjectName} \\033[1;36m${currentBuild.displayName} \\033[1;32mstaged for deploy after \$((\$(date +%s%N)/1000000-${currentBuild.startTimeInMillis}))ms\\033[m.\" >> ~/builds.txt"
+		sh "echo -e \"[\\033[0;32m\$(date +%T)\\033[m] \\033[0;36m${currentBuild.fullProjectName} \\033[1;36m${currentBuild.displayName} \\033[1;32mstaged for deploy after \$((\$(date +%s%N)/1000000-${currentBuild.startTimeInMillis}))ms\\033[m.\" >> ~/builds.txt"
 	}
 }
